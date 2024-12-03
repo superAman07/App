ef7471423badba2be03d9291e24864705387cd56
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-12-04 00:42:12 +0530

    Delete Backend/src directory

--- Backend/src/generateSecret.ts
+++
@@ -1,8 +0,0 @@
-import crypto from 'crypto';
-
-const generateSecret = () => {
-  const secret = crypto.randomBytes(64).toString('hex');
-  console.log('Generated JWT Secret:', secret);
-};
-
-generateSecret();
